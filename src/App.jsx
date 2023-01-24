939f4660cd4b192a56558d172167e1136e4c8857
Jonatha Targino <j.oliveiratargino@hotmail.com>
2023-01-24 20:43:02 -0300

    layout finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
+import Footer from './components/Footer'
+import MainContent from './components/MainContent'
+import NavBar from './components/NavBar'
 
 function App() {
 
   return (
-    <>
-    </>
+    <div className='AppDiv'>
+      <NavBar/>
+      <MainContent />
+      <Footer/>
+    </div>
   )
 }
 
