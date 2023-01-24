1574ac2cfa8f661a2ed958f628064b3f3d2fe288
Jonatha Targino <j.oliveiratargino@hotmail.com>
2023-01-24 11:53:15 -0300

    MainContent's product info done

---
+++ src/components/MainContent/index.jsx
@@ -0,0 +1,21 @@
+import styles from "./styles.module.css"
+
+function MainContent(){
+  return (
+    <div className={styles.mainGrid}>
+      <div className={styles.productInfo}>
+        <div className={styles.centered}>
+          <h1 className={styles.title}>Tênis Esportivo Para Corrida Top</h1>
+          <h2 className={styles.subtitle}>Esse tênis tem o intuito de te deixar ainda mais fitness, focado e musculoso.</h2>
+          <span className={styles.price}>R$ 1.000,00</span>
+          <button className={styles.buyButton} type="button">Comprar</button>
+        </div>
+      </div>
+      <div className={styles.productPhotos}>
+
+      </div>
+    </div>
+  )
+}
+
+export default MainContent;
