0a58cfc462e7a37206f05e575e89d5815033cb91
Jonatha Targino <j.oliveiratargino@hotmail.com>
2023-01-24 15:04:12 -0300

    Footer component created

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,20 @@
+import imgVideo from "../../assets/img-video.png"
+
+import styles from "./styles.module.css"
+
+function Footer(){
+  return (
+    <div className={styles.footer}>
+      <img src={imgVideo} className={styles.img}/>
+      <div className={styles.socialMedia}>
+        <span>Facebook</span>
+        <span>Instagram</span>
+        <span>Youtube</span>
+        <span>Twitter</span>
+      </div>
+      <button className={styles.exploreButton} type="button">Explore mais</button>
+    </div>
+  )
+}
+
+export default Footer
