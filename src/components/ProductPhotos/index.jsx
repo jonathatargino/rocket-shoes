d925b0785bb9c298b392822f7310c142e419cee5
Jonatha Targino <j.oliveiratargino@hotmail.com>
2023-01-25 08:28:49 -0300

    auto slide photos added

--- src/components/ProductPhotos/index.jsx
+++ src/components/ProductPhotos/index.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect, useRef} from "react"
+
 import tenis1 from "../../assets/tenis-1-galeria.png"
 import tenis2 from "../../assets/tenis-2-galeria.png"
 import tenis3 from "../../assets/tenis-3-galeria.png"
@@ -8,9 +10,34 @@ import tenisAmpliado3 from "../../assets/tenis-3-ampliado.png"
 import styles from "./styles.module.css"
 
 function ProductPhotos(){
+  const [selectedImage, setSelectedImage] = useState(0)
+  const imgArray = [tenisAmpliado1, tenisAmpliado2, tenisAmpliado3]
+  const autoPlayRef = useRef()
+  function updateSelectedImage(){
+    if (selectedImage === 2){
+      setSelectedImage(0);
+    }
+    else {
+      setSelectedImage(selectedImage + 1);
+    }
+  }
+
+  useEffect(() => {
+    autoPlayRef.current = updateSelectedImage
+  })
+
+  useEffect(() => {
+    const play = () => {
+      autoPlayRef.current()
+    }
+
+    const interval = setInterval(play, 2000);
+  },[])
+
+
   return (
     <div className={styles.productPhotos}>
-      <img src={tenisAmpliado2}  className={styles.mainImg}/>
+      <img src={imgArray[selectedImage]}  className={styles.mainImg}/>
       <div className={styles.imgGrid}>
         <img src={tenis1} className={styles.bottomImg}/>
         <img src={tenis2} className={styles.bottomImg}/>
