995ac3292aab3163c2d83361f8d660738d31519d
Jonatha Targino <j.oliveiratargino@hotmail.com>
2023-01-25 20:08:15 -0300

    responsive navbar

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,20 +1,26 @@
+import { useEffect, useState } from "react"
+
 import logo from "../../assets/logo.svg"
 import bagSvg from "../../assets/icon-bag.svg"
 
 import SearchInput from "../../components/SearchInput"
 
 import styles from "./styles.module.css"
+import NavBarOptions from "../NavBarOptions"
+import MenuIcon from "../MenuIcon"
 
 function NavBar(){
+  const [showOptions, setShowOptions] = useState(false)
+
   return (
-    <div className={styles.navbar}>
+    <div 
+    className={styles.navbar} 
+    >
       <img src={logo}/>
-      <span>Homem</span>
-      <span>Mulher</span>
-      <span>Criança</span>
-      <span>Customizar</span>
+      <NavBarOptions showOptions={showOptions}/>
       <SearchInput/>
       <img src={bagSvg} className={styles.bag}/>
+      <MenuIcon showOptions={showOptions} setShowOptions={setShowOptions}/>
     </div>
   )
 }
